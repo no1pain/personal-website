0a26b805ebe36cd479160e217f1ea381ed379299
Oleksandr <fe.oleksandr.kazan@gmail.com>
2026-07-16 20:19:21 +0300

    Add disallow rules for API and admin routes in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: ['/api/', '/admin/'],
     },
     sitemap: 'https://oleksandr.dev/sitemap.xml',
   }
